2b691ddbe2630ce636f99a4cffd786b6914f19aa
yash-wq <yash.vishwakarma007@gmail.com>
2021-12-02 13:06:35 +0530

    complete V1

---
+++ config.py
@@ -0,0 +1,39 @@
+
+import os
+import sys
+env_name = input("Enter the name of the virtual environment: ")
+domain = input("Enter the domain you have configured to the IP: ")
+user = sys.argv[1]
+project_name = sys.argv[2]
+
+service_file_contents = f"""
+[Unit]
+Description=Gunicorn instance to serve myproject
+After=network.target
+
+[Service]
+User={user}
+Group=www-data
+WorkingDirectory=/home/{user}/{project_name}
+Environment="PATH=/home/{user}/{project_name}/{env_name}/bin"
+ExecStart=/home/{user}/{project_name}/{env_name}/bin/gunicorn --workers 1 --bind unix:{project_name}.sock -m 007 app:app
+
+[Install]
+WantedBy=multi-user.target
+"""
+with open(f"nginx-config/{project_name}.service", 'w') as f:
+    f.write(service_file_contents)
+
+nginx_contents = """
+server {
+    listen 80;
+    server_name """ + domain + """ ;
+
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/home/"""+user+"""/"""+project_name+"""/"""+project_name+""".sock;
+    }
+}
+"""
+with open(f"nginx-config/{project_name}", 'w') as f:
+    f.write(nginx_contents)
--- nginx-config.sh
+++ nginx-config.sh
@@ -1,5 +1,26 @@
+#!/bin/bash
 sudo apt update
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-pip install requirements.txt
+pip install -r requirements.txt
 pip install gunicorn
-gunicorn --bind 0.0.0.0:8000 app:app
+sudo apt install nginx
+
+user=$(whoami)
+
+project_name=$(pwd | grep -o '[^/]*$')
+
+python3 nginx-config/config.py $user $project_name
+service_file_name="${project_name}.service"
+nginx_file_name="${project_name}"
+echo $service_file_name
+echo $nginx_file_name
+sudo cp "nginx-config/$service_file_name" "/etc/systemd/system/${project_name}.service"
+sudo systemctl daemon-reload
+sudo systemctl start $project_name
+sudo systemctl enable $project_name
+
+sudo cp "nginx-config/$nginx_file_name" "/etc/nginx/sites-available/${project_name}"
+sudo ln -s "/etc/nginx/sites-available/${project_name}" /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+sudo ufw delete allow 8000
+sudo ufw allow 'Nginx Full'
