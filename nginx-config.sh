62b7c834df175efcc5f03e36cbd6d5c53d898c4b
Yash Vishwakarma <67466264+yash-wq@users.noreply.github.com>
2024-04-10 14:31:25 -0400

    Update nginx-config.sh

--- nginx-config.sh
+++ nginx-config.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 pip install -r requirements.txt
-pip install gunicorn
+pip install gunicorn #pip install
 sudo apt install nginx
 
 user=$(whoami)
